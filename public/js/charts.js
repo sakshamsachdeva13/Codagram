1506b5e63d816b6de417a86f1c078f1933420d6b
saksham sachdeva <saksham5sachdeva@gmail.com>
2018-07-23 22:33:28 +0530

    df

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,7 +1,7 @@
 
 
 
-fetch('http://secure-sierra-40015.herokuapp.com/profilepage/user')
+fetch('https://secure-sierra-40015.herokuapp.com/profilepage/user')
 .then(res => res.json())
 .then(data => {
   console.log(data.data)
